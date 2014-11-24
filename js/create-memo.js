c2e6cbdd879a80c5c9431bb6dd34c50cedba32d4
ulisescontreras <indieuli@outlook.com>
2014-11-24 14:30:01 +0000

    Updating .gitignore file

---
+++ js/create-memo.js
@@ -0,0 +1,39 @@
+window.onload = function(){
+
+var add =  document.getElementById('create-memo');
+var remove = document.getElementById('remove-memo');
+
+function toggleList(event){
+	event.target.classList.toggle('strikethrough');
+}
+
+function newMemo(list, itemText){
+	var listMemo = document.createElement('li');
+	listMemo.className = 'memo';
+	listMemo.innerText = itemText;
+	list.appendChild(listMemo);
+
+  listMemo.addEventListener('click', toggleList);
+}
+
+add.onclick = function(){
+	var itemText = prompt('Create a Memo');
+		if(itemText) {
+		  newMemo(document.getElementById('content-memo'), itemText);
+	  }else {
+	  	return false;
+	  }
+
+}
+
+function removeList(){
+	var lists = document.querySelectorAll('li.strikethrough');
+
+	for (var i = 0; i < lists.length; i++) {
+		lists[i].parentNode.removeChild(lists[i]);
+	}
+}
+
+	remove.addEventListener('click', removeList);
+
+}
---
+++ js/geolocation.js
@@ -0,0 +1,41 @@
+window.addEventListener("domready", geolocation, false);
+
+var search = document.getElementById('geolocation-search');
+var getMap = document.getElementById('map');
+
+//function toggleList(event){
+//	event.target.classList.toggle('displayMap');
+//}
+
+search.addEventListener('click', getGeolocation, false);
+
+function getGeolocation(){
+	if(navigator.geolocation) {
+		 navigator.geolocation.getCurrentPosition(geolocation, handlingErrors);
+  } else {
+		alert('Your browser does not support geolocator');
+}
+}
+
+function geolocation(position){
+var latitud = position.coords.latitude;
+var longitud = position.coords.longitude;
+var map = 'http://maps.google.com/maps/api/staticmap?center=' + latitud + ',' + longitud + '&zoom=15&size=400x200&markers=color:blue|label:A|' + latitud + ',' + longitud + '&sensor=false';
+  getMap.innerHTML = "<img src='"+ map +"'>";
+}
+
+function handlingErrors (err) {
+
+if (err.code == 0) {
+	alert('You need authorize geolocation in your browser');
+}
+
+else if (err.code == 1) {
+	alert('Your position is not available');
+}
+
+else if (err.code == 2) {
+	alert('Your browser has blocked the geolocation');
+}
+}
+
